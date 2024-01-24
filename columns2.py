da3f35e2ec0e2679898a6a503b662d148fd2992b
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-01-24 11:48:19 +0700

    Update columns2.py

--- columns2.py
+++ columns2.py
@@ -9,15 +9,15 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
    st.header("Versicolor")
-   st.image("https://en.m.wikipedia.org/wiki/File:Iris_versicolor_3.jpg")
+   st.image("./img2/Iris_versicolor_3.jpg")
 
 with col2:
    st.header("Verginica")
-   st.image("https://uk.m.wikipedia.org/wiki/%D0%A4%D0%B0%D0%B9%D0%BB:Iris_virginica.jpg")
+   st.image("./img2/verginca.jpg")
 
 with col3:
    st.header("Setosa")
-   st.image("https://en.m.wikipedia.org/wiki/File:Iris_setosa.JPG")
+   st.image("./img2/Iris_setosa.JPG")
 
 #import pandas as pd
 df=pd.read_csv("./data/iris.csv")
@@ -28,11 +28,9 @@ if(st.button("แสดงข้อมูลตัวอย่าง")):
 else:
     st.button("ไม่แสดงข้อมูลตัวอย่าง")
 
-if(st.button("แสดงข้อมูลสถิติ")):
-    #สรุปตามค่าเฉลี่ย กราฟแท่ง
-    st.write(df.groupby('variety').mean())
-    #chart_data=df.groupby('variety').mean()
-    #chart_data.columns
+
+
+if(st.button("แสดงกราฟแท่ง")):
     chart_data = pd.DataFrame(
     {
         "ประเภทดอกไม้": df['variety'],
@@ -41,36 +39,26 @@ if(st.button("แสดงข้อมูลสถิติ")):
         }
     )
     st.bar_chart(chart_data, x="ประเภทดอกไม้", y=["ความกว้าง","ความยาว"], color=["#FF0000", "#0000FF"])
-    st.button("ไม่แสดงข้อมูลสถิติ")
+    st.button("ไม่แสดงกราฟแท่ง")
 else:
-    st.button("ไม่แสดงข้อมูลสถิติ")
+    st.button("ไม่แสดงกราฟแท่ง")
 
 import matplotlib.pyplot as plt
 
-# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+# Pie 
 labels = 'sepal.width', 'sepal.length', 'petal.width', 'petal.length'
+
 x1=df['sepal.width'].mean()
 x2=df['sepal.length'].mean()
 x3=df['petal.width'].mean()
 x4=df['petal.length'].mean()
+
 sizes = [x1, x2, x3, x4]
+
 explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 #ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
 st.pyplot(fig1)
-
-chart = alt.Chart(df).mark_tick().encode(
-        x='Horsepower:Q',
-        y='Cylinders:O'
-    )
-
-tab1, tab2 = st.tabs(["Streamlit theme (default)", "Altair native theme"])
-
-    with tab1:
-        st.altair_chart(chart, theme="streamlit", use_container_width=True)
-    with tab2:
-        st.altair_chart(chart, theme=None, use_container_width=True)
